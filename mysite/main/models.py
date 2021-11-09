c9a75f1f3c1326621db405989ed1caad22f7d395
Joseph Levin <joseph.levin99@gmail.com>
2021-11-08 23:35:48 -0800

    Defined models

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -4,9 +4,18 @@ from django.contrib.auth.models import User
 
 class HouseholdModel(models.Model):
   name = models.CharField(max_length=255)
-  members = models.ManyToManyField(User)
-  lists = models.ManyToManyField()
+
+class HouseholdMemberModel(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user')
+  household = models.ForeignKey(HouseholdModel, on_delete=models.CASCADE, related_name='member')
 
 class ListModel(models.Model):
-  name = models.CharField(max_length=255)
-  household = models.ForeignKey(HouseholdModel, related_name='household_list')
+  name = models.CharField(max_length=255, unique=True)
+  household = models.ForeignKey(HouseholdModel, on_delete=models.CASCADE, related_name='list')
+
+class ListItemModel(models.Model):
+  description = models.CharField(max_length=255)
+  price = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+  list = models.ForeignKey(ListModel, on_delete=models.CASCADE, related_name='item')
+  author = models.ForeignKey(HouseholdMemberModel, on_delete=models.CASCADE, related_name='item')
+  
